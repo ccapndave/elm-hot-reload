222b9a8201595f200b6109d19f505af28a584f93
Dave Keen <dev@ruffness.com>
2016-04-28 17:47:55 +0200

    A new working version for Elm 0.17

--- src/HotReload.elm
+++ src/HotReload.elm
@@ -1,23 +1,45 @@
-module HotReload where
+module HotReload exposing (setupInit, setupInitWithFlags, setupUpdate)
 
-import StartApp
-import Effects exposing (Effects)
 import Native.HotReload
+import Task exposing (Task, andThen, succeed, sequence, map)
 
-{-| Get the init for use in StartApp.  If there is a saved state that will be used, otherwise the
-provided default state will be used.
+{-| Activate hot reloading on init for a program created using Html.program
 -}
-init : (a, Effects b) -> (a, Effects b)
-init default =
+setupInit : Maybe (msg, msg) -> (model, Cmd msg) -> (model, Cmd msg)
+setupInit msgs default =
   Native.HotReload.init ()
-    |> Maybe.map (\model -> (model, Effects.none))
+    |> Maybe.map (\restoredModel -> (restoredModel, hotReloadedCmd msgs))
     |> Maybe.withDefault default
 
 
-{-| Create a port for saving the model when it changes
+{-| Active hot reloading on init for a program created using Html.programWithFlags
 -}
-hotReloadPort : StartApp.App a -> Signal ()
-hotReloadPort app =
-  app.model
-    |> Signal.map Native.HotReload.saveModel
-    |> Signal.map (always ())
+setupInitWithFlags : Maybe (msg, msg) -> (flags -> (model, Cmd msg)) -> (flags -> (model, Cmd msg))
+setupInitWithFlags msgs default =
+  Native.HotReload.init ()
+    |> Maybe.map (\restoredModel -> always (restoredModel, hotReloadedCmd msgs))
+    |> Maybe.withDefault default
+
+
+{-| Activate hot reloading on update
+-}
+setupUpdate : (msg -> model -> (model, Cmd msg)) -> (msg -> model -> (model, Cmd msg))
+setupUpdate default =
+  \msg model ->
+    let
+      -- Call the real update to get out the model and cmd
+      (model, cmd) =
+        default msg model
+
+      -- Use the Native function to save the model as a side-effect
+      _ =
+        Native.HotReload.saveModel model
+    in
+    (model, cmd)
+
+
+hotReloadedCmd : Maybe (msg, msg) -> Cmd msg
+hotReloadedCmd msgs =
+  msgs
+    |> Maybe.map (\(msg, noopMsg) -> Task.perform (always noopMsg) (always msg) (Task.succeed ()))
+    |> Maybe.withDefault Cmd.none
--- src/Native/HotReload.js
+++ src/Native/HotReload.js
@@ -1,30 +1,25 @@
-Elm.Native = Elm.Native || {};
-Elm.Native.HotReload = {};
-Elm.Native.HotReload.make = function(localRuntime) {
-  localRuntime.Native = localRuntime.Native || {};
-  localRuntime.Native.HotReload = localRuntime.Native.HotReload || {};
-  if (localRuntime.Native.HotReload.values) return localRuntime.Native.HotReload.values;
+//import Maybe //
 
-  var Maybe = Elm.Maybe.make(localRuntime);
-  var Signal = Elm.Native.Signal.make(localRuntime);
+//var _ccapndave$elm_hot_reload$Native_HotReload = function() {
+var _user$project$Native_HotReload = function() {
 
-  var init = function init(cb) {
+  function init() {
     var state = localStorage.getItem("state");
 
     if (state) {
       console.info("The model was restored from localStorage! If you find yourself getting strange errors try clearing the state using localStorage.removeItem('state') and reloading the page.");
-      return Maybe.Just(JSON.parse(state));
+      return _elm_lang$core$Maybe$Just(JSON.parse(state));
     } else {
-      return Maybe.Nothing;
+      return _elm_lang$core$Maybe$Nothing;
     }
   };
 
-  var saveModel = function saveModel(model) {
+  function saveModel(model) {
     localStorage.setItem("state", JSON.stringify(model));
   };
 
-  return localRuntime.Native.HotReload.values = {
+  return {
     init: init,
     saveModel: saveModel
-  };
-};
+  }
+}();
